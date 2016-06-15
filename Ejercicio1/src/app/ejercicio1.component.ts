4a908f576084ba09b8a87d2ca8b9ea613f9ba90f
MelitonTienda <met.avila@outlook.com>
2016-06-14 21:45:25 -0500

    Modulo2

--- Ejercicio1/src/app/ejercicio1.component.ts
+++ Ejercicio1/src/app/ejercicio1.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import	{MyComponent1} from './modules/module1/component1.component'
+import	{MyComponent2} from './modules/module2/component2.component'
 
 @Component({
   moduleId: module.id,
   selector: 'ejercicio1-app',
   templateUrl: 'ejercicio1.component.html',
   styleUrls: ['ejercicio1.component.css'],
-  directives:[MyComponent1]
+  directives:[MyComponent1,MyComponent2]
 })
 export class Ejercicio1AppComponent {
  title = 'ejercicio1 funcionando!.';
---
+++ Ejercicio1/src/app/modules/module2/component2.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'Componente-2',
+    template:'This is my second component 2, Awesome.'
+})
+
+export class MyComponent2
+{
+
+}
