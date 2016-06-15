82230cda34a7c4944919b55051270ed396eea234
MelitonTienda <met.avila@outlook.com>
2016-06-14 21:28:14 -0500

    Agregado modulo 1

--- Ejercicio1/src/app/ejercicio1.component.ts
+++ Ejercicio1/src/app/ejercicio1.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import	{MyComponent1} from './modules/module1/component1.component'
 
 @Component({
   moduleId: module.id,
   selector: 'ejercicio1-app',
   templateUrl: 'ejercicio1.component.html',
-  styleUrls: ['ejercicio1.component.css']
+  styleUrls: ['ejercicio1.component.css'],
+  directives:[MyComponent1]
 })
 export class Ejercicio1AppComponent {
-  title = 'ejercicio1 works!';
+ title = 'ejercicio1 funcionando!.';
 }
