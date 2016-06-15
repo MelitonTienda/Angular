d8ca058d510c568d26b70319889cdd455a838644
MelitonTienda <met.avila@outlook.com>
2016-06-15 16:42:13 -0500

    Cambio

--- Ejercicio1/src/app/modules/module1/component1.component.ts
+++ Ejercicio1/src/app/modules/module1/component1.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'Componente-1',
-    template:'This is my firts component 1, Rocks.'
+    template:'This is my firts component 1, Awesome.'
 })
 
 export class MyComponent1
