560b5020124c3d626c5ce3a95c7a2f6d83bcd841
MelitonTienda <met.avila@outlook.com>
2016-06-15 23:39:23 -0500

    Modificaciones

--- Ejercicio1/src/app/modules/module1/component1.component.ts
+++ Ejercicio1/src/app/modules/module1/component1.component.ts
@@ -1,11 +1,19 @@
 import {Component} from '@angular/core';
 
 @Component({
+    moduleId:module.id,
     selector:'Componente-1',
-    template:'This is my firts component 1, Awesome.'
+    template:`This is my firts component 1. Nombre: <span [style.color]="inputElement.value === 'yes' ? 'blue': 'red'">{{name}}</span>.
+    <span [class.is-awesome]="inputElement.value === 'yes'">It´s awasome!</span>
+    <br>
+    <br>
+    Is it awasome?
+    <input type="text" #inputElement (keyup)="0">
+    <button [disabled]="inputElement.value !== 'yes'">Only enabled if "yes" was entered</button>
+    `, styleUrls: ['component1.css']
 })
 
 export class MyComponent1
 {
-
+    name: string='Melitón';
 }
